c0272e510dcb65d777b07a0269173ddc12379059
Jamie Chen <jamiechenang@gmail.com>
2023-12-15 00:15:19 -0500

    Update client.go

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -12,35 +12,37 @@ func main() {
 
 	// Create a new HTTP client
 	client := http1.NewClient()
-	client.SetKeepAlive(false, 0 , 0)
+	client.SetKeepAlive(false, 0, 0)
 
 	// Send a GET request to the server
+	fmt.Println("[INFO] Sending Get request for /index.html")
 	resp, err := client.Get("http://127.0.0.1:80/")
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("[ERROR] Failed to Get /index.html:", err)
 		return
 	}
 	defer resp.Body.Close()
 	// Print the response
-	fmt.Println("Response:")
-	resp.Write(os.Stdout)
-	fmt.Println()
+	fmt.Println("[INFO] Response:")
+	resp.Header.Write(os.Stdout)
+	fmt.Println("(Content body omitted)\n")
 
-	// wait for 30 seconds
+	// Wait for a while
 	timer := time.NewTicker(15 * time.Second)
 	<-timer.C
 
 	client.SetKeepAlive(true, 0, 0)
 	// Send a GET request to the server with basic authentication
+	fmt.Println("[INFO] Sending Get request for /admin")
 	resp, err = client.Get("http://127.0.0.1:80/admin")
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("[ERROR] Failed to Get /admin:", err)
 		return
 	}
 	defer resp.Body.Close()
 
 	// Print the response
-	fmt.Println("Response:")
+	fmt.Println("[INFO] Response:")
 	resp.Write(os.Stdout)
 
 }
